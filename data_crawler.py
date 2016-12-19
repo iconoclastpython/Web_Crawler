0a2edb6a71246bbc1e9b92535636e6a7d89404a2
iconoclastpython <ico.yigang@hotmail.com>
2016-12-19 18:31:51 -0500

    multi-process finished

--- data_crawler.py
+++ data_crawler.py
@@ -17,11 +17,9 @@ Packages:
 *** brew ***
 1. rabbitmq;
 
-
 *** pip ***
 1. BeautifulSoup;
 2. pika;
-
 
 '''
 
@@ -29,8 +27,15 @@ Packages:
 from bs4 import BeautifulSoup
 import requests
 
+from multiprocessing import Pool
+from multiprocessing import Process
+
+import time
+
+start_time = time.time()
 DOWNLOAD_URL = 'https://techcrunch.com'
-page = 1
+urls = [DOWNLOAD_URL]
+
 
 def download_page(url):
   # 1. Modify U-A more powerful here 
@@ -39,9 +44,19 @@ def download_page(url):
   # 2. Get JSON file if neccessary
   return page
 
-def parse_html(url, page):
+def groom_url(url):
+  page = download_page(url)
+  soup = BeautifulSoup(page)
+  soup_next = soup.find('div', attrs={'class': 'river-end'}).find_all('a')
+  for next_url in soup_next:
+    if next_url.getText() == 'Next':
+      urls.append(DOWNLOAD_URL + next_url.get('href'))
+      print 'Get url: %s'%DOWNLOAD_URL + next_url.get('href') 
+      return DOWNLOAD_URL + next_url.get('href')
+
+def parse_html(url):
   print "-----------------"
-  print "Page: %d"%page
+  print "Page: %s"%url
   page = download_page(url)
   soup = BeautifulSoup(page)
   soup_list = soup.find('ul', attrs={'class': 'river lc-padding', 'id': 'river1'})
@@ -49,7 +64,7 @@ def parse_html(url, page):
     for article in article_list.find_all('h2'):
       block = article.find('a')
       title = block.getText()
-      print title
+      print url, "-->", title
 
       sub_link = block.get('href')
       sub_page = download_page(sub_link)
@@ -59,21 +74,31 @@ def parse_html(url, page):
         detail = "Timestamp: " + target.find('time').get('datetime')
       except:
         pass
-      print detail
+      print url, "-->", detail
       print '\n'
 
-  soup_next = soup.find('div', attrs={'class': 'river-end'}).find_all('a')
-  for next_url in soup_next:
-    if next_url.getText() == 'Next':
-      return DOWNLOAD_URL + next_url.get('href')
 
 def main():
-  url = DOWNLOAD_URL
-  cur_page = page
-  page_limit = 5
-  while url and page_limit >= cur_page:
-    url = parse_html(url, cur_page)
+  # Get all urls
+  cur_page = 1
+  page_limit = 10
+  index = 0
+  while page_limit > cur_page:
+    groom_url(urls[index])
+    index += 1
     cur_page += 1
+  print "url list: ", urls
+
+  # Multi-Process crawler
+  pool = Pool(10)
+  pool.map(parse_html, urls)
+  pool.close()
+  pool.join()
+
+  print time.time() - start_time
+
+  # Here disorder print should be save with keys in database
+  # Better to save url or html content in a queue, but avoid OOM
 
 if __name__ =='__main__':
   main()
